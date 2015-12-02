3a859c84f54c21a94c52c91dc033e9f21faef361
JihoonPark <grinbee777@naver.com>
2015-12-02 19:06:57 +0900

    [박지훈] 1.서버 안돌아가서 중간꺼 업데이트 Test....

--- DodamProject/src/main/java/com/dodam/dao/DailyStateDao.java
+++ DodamProject/src/main/java/com/dodam/dao/DailyStateDao.java
@@ -2,13 +2,12 @@ package com.dodam.dao;
 
 import java.util.List;
 
-import com.dodam.common.DailyStateSearch;
 import com.dodam.service.domain.DailyState;
 
 public interface DailyStateDao {
-	int insertDState(int bNo);
+	int insertDState(DailyState dailyState);
 	//dsNo, 날짜 전달
-	DailyState getDailyState(DailyStateSearch dailyStateSearch);
+	DailyState getDailyState(DailyState dailyState);
 	List<DailyState> getDailyStateList(int bNo);
 	//dsNo or bNo 전달
 	int deleteDailyState(int no); 
