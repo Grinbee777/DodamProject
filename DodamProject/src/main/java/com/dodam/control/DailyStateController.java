122512b89f3f635ad488106069441b2d334a6a06
JihoonPark <grinbee777@naver.com>
2015-12-03 16:28:56 +0900

    [박지훈] 1.DailyState/Nap/Poo/Feed 로직 전체 수정중 2.에러 해결을 위한 업로드

--- DodamProject/src/main/java/com/dodam/control/DailyStateController.java
+++ DodamProject/src/main/java/com/dodam/control/DailyStateController.java
@@ -1,6 +1,5 @@
 package com.dodam.control;
 
-import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -10,7 +9,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.dodam.service.DailyStateService;
+import com.dodam.service.FeedService;
+import com.dodam.service.NapService;
+import com.dodam.service.PooService;
 import com.dodam.service.domain.DailyState;
+import com.dodam.service.domain.Feed;
+import com.dodam.service.domain.Nap;
+import com.dodam.service.domain.Poo;
 
 @Controller
 @RequestMapping("/dailyState/*")
@@ -20,21 +25,113 @@ public class DailyStateController {
 	@Qualifier("dailyStateServiceImpl")
 	private DailyStateService dailyStateService;
 	
+	@Autowired
+	@Qualifier("napServiceImpl")
+	private NapService napService;
+	
+	@Autowired
+	@Qualifier("feedServiceImpl")
+	private FeedService feedService;
+	
+	@Autowired
+	@Qualifier("pooServiceImpl")
+	private PooService pooService;
+	
 	public DailyStateController() {
 		System.out.println(":::::"+getClass().getName()+" 생성!");
 	}
 	
-	@RequestMapping( value="/json/getDailyState")
+	
+	@RequestMapping(value="/json/addNap")
+	public void addJsonNap(@RequestBody DailyState dailyState, @RequestBody Nap nap) throws Exception {
+		
+		if(dailyState.getDsNo()==0){
+			dailyStateService.insertDState(dailyState);
+		}else{
+			
+		}
+		napService.insertNap(nap);
+	}
+	
+	/*
+	@RequestMapping(value="/json/updateNap")
+	public void updateJsonNap(@RequestBody Nap nap) throws Exception{
+	}
+	*/
+	
+	@RequestMapping(value="/json/")
+	public void n2() throws Exception{
+		
+	}
+	
+	@RequestMapping(value="/json/")
+	public void n3() throws Exception{
+		
+	}
+	
+	
+	
+	@RequestMapping(value="/json/addFeed")
+	public void addJsonFeed(@RequestBody DailyState dailyState, @RequestBody Feed feed) throws Exception {
+		//check 로직 추가 - DS 도메인 or dsNo
+		feedService.insertFeed(feed);
+	}
+	
+	@RequestMapping(value="/json/")
+	public void f1() throws Exception{
+		
+	}
+	
+	@RequestMapping(value="/json/")
+	public void f2() throws Exception{
+		
+	}
+	
+	@RequestMapping(value="/json/")
+	public void f3() throws Exception{
+		
+	}
+	
+	@RequestMapping(value="/json/addPoo")
+	public void addJsonPoo(@RequestBody DailyState dailyState, @RequestBody Poo poo) throws Exception {
+		//check 로직 추가 - DS 도메인 or dsNo
+		pooService.insertPoo(poo);
+	}
+	
+	@RequestMapping(value="/json/")
+	public void p1() throws Exception{
+		
+	}
+	
+	@RequestMapping(value="/json/")
+	public void p2() throws Exception{
+		
+	}
+	
+	@RequestMapping(value="/json/")
+	public void p3() throws Exception{
+		
+	}
+	
+	@RequestMapping(value="/json/getDailyState")
 	public void getJsonDailyState(@RequestBody DailyState dailyState, Model model) throws Exception {
-		dailyState.setbNO(100011);
-		Date d = new Date(2015, 12, 02);
-		dailyState.setDsDate(d);
+		System.out.println("@@ "+dailyState);
+		//model.addAttribute(dailyStateService.getDailyState(dailyState));
+	}
+	
+	@RequestMapping(value="/json/")
+	public void d1() throws Exception{
 		
-		dailyState = dailyStateService.getDailyState(dailyState);
+	}
+	
+	@RequestMapping(value="/json/")
+	public void d2() throws Exception{
 		
-		System.out.println("####"+dailyState);
+	}
+	
+	@RequestMapping(value="/json/")
+	public void d3() throws Exception{
 		
-		model.addAttribute(dailyState);
 	}
 
 }
