9bd41a9ff270d16ca0a827837e4ef0584a984052
ksy <ksy5704@gmail.com>
2015-12-03 20:53:04 +0900

    [김시용] 1. DiaryController 진행중

--- DodamProject/src/main/java/com/dodam/control/DiaryController.java
+++ DodamProject/src/main/java/com/dodam/control/DiaryController.java
@@ -30,6 +30,43 @@ public class DiaryController {
 		System.out.println("전달받은 diary 인스턴스 == "+diary);
 		diaryService.insertDiary(diary);
 		
+	}
+	
+	@RequestMapping(value="/json/updateDiary", method=RequestMethod.POST)
+	public void updateJsonDiary(@RequestBody Diary diary, Model model) throws Exception{
+		
+		System.out.println(":: updateJsonDiary ::");
+		System.out.println("전달받은 diary 인스턴스 == "+diary);
+		diaryService.updateDiary(diary);
+		
+	}
+	
+	@RequestMapping(value="/json/deleteDiary", method=RequestMethod.POST)
+	public void deleteJsonDiary(@RequestBody Diary diary, Model model) throws Exception{
+		
+		System.out.println(":: updateJsonDiary ::");
+		System.out.println("전달받은 diary 인스턴스 == "+diary);
+		diaryService.deleteDiary(diary.getdNo());
+		
+	}
+	
+	@RequestMapping(value="/json/getDiary", method=RequestMethod.POST)
+	public void getJsonDiary(@RequestBody Diary diary, Model model) throws Exception{
+		
+		System.out.println(":: getJsonDiary ::");
+		System.out.println("전달받은 diary 인스턴스 == "+diary);
+		Diary returnDiary = diaryService.getDiary(diary.getdNo());
+		System.out.println("리턴될 diary : "+returnDiary);
+		model.addAttribute("diary", returnDiary);
+		
+	}
+	
+	@RequestMapping(value="/json/getUserDiary", method=RequestMethod.POST)
+	public void getJsonUserDiary(@RequestBody Diary diary, Model model) throws Exception{
+		
+		System.out.println(":: getJsonDiaryList ::");
+		System.out.println("전달받은 diary 인스턴스 == "+diary);
+		diaryService.getDiaryList(diary.getDiaryUser().getuNo());
 		
 	}
 	
