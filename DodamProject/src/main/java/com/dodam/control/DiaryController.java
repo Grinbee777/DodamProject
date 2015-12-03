0fa2528656d8718eb8549ff9d702803d245dc883
ksy <ksy5704@gmail.com>
2015-12-03 11:23:08 +0900

    [김시용] 1. dipatch - servlet.xml 에 jackson2 설정 추가

--- DodamProject/src/main/java/com/dodam/control/DiaryController.java
+++ DodamProject/src/main/java/com/dodam/control/DiaryController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.dodam.service.DiaryService;
 import com.dodam.service.domain.Diary;
@@ -22,7 +23,7 @@ public class DiaryController {
 		System.out.println(":::::"+getClass().getName()+" 생성!");
 	}
 	
-	@RequestMapping(value="/json/addDiary")
+	@RequestMapping(value="/json/addDiary", method=RequestMethod.POST)
 	public void addJsonDiary(@RequestBody Diary diary, Model model) throws Exception{
 		
 		System.out.println(":: addJsonDiary ::");
