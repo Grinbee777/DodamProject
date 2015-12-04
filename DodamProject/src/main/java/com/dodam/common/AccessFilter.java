595de8e77ec055362fb94c16b8039a4d970cefc6
ksy <ksy5704@gmail.com>
2015-12-04 14:40:24 +0900

    [김시용] 1. 필터추가

---
+++ DodamProject/src/main/java/com/dodam/common/AccessFilter.java
@@ -0,0 +1,39 @@
+package com.dodam.common;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter("/*")
+public class AccessFilter implements Filter {
+//	ũ�ν� ������ ������ �ذ��ϱ����� Access����
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		((HttpServletResponse)response).setHeader("Access-Control-Allow-Origin", "*");
+		
+		chain.doFilter(request, response);
+		
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
